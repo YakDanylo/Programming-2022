f027cf1d511d633a357e5f7ec41ae1ddb637b834
YakDanylo <alatp574@gmail.com>
2022-10-02 17:47:21 +0300

    Add "Homework 05.10"

--- Homework/Homework/Homework/Source.cpp
+++ Homework/Homework/Homework/Source.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 
 int main()
@@ -6,9 +7,76 @@ int main()
 	setlocale(LC_ALL, "Ukr");
 	/*cout << "Введiть секунду" << endl;
 	int sec;
-	cin >> sec;                                   Завдання 1
+	cin >> sec;                                   Завдання 1 (1)
 	cout << "Година: " << sec / 3600 << endl;
 	cout << "Хвилина: " << sec / 60 << endl;*/
 
+	/*cout << "enter x: " << endl;
+	int x;
+	cin >> x;
+	cout << "function y=x^2" << endl;
+	cout << "x = " << x << endl;                    завдання 2 (9а)
+	cout << "y = " << x * x<<endl;
+	cout << "___________________" << endl;
+	cout << "function y=-x" << endl;
+	cout << "x = " << x << endl;
+	cout << "y = "<< - x << endl;
+	cout << "___________________" << endl;*/
+	
+	/*cout << "enter x: " << endl;
+	int x;               
+	cin >> x;                                       завдання 3(12г)
+	cout << "function y=1-x^3" << endl;
+	cout << "x = " << x << endl;
+	cout << "y = " << 1 - pow(x,3);*/
 
+
+	/*cout << "Enter a month" << endl;
+	int x;
+	cin >> x;
+	switch (x)
+	{                                          завдання 4(15)
+	case 1: cout << "January";
+		break;
+	case 2: cout << "February";
+		break;
+	case 3: cout << "March";
+		break;
+	case 4: cout << "April";
+		break;
+	case 5: cout << "May";
+		break;
+	case 6: cout << "June";
+		break;
+	case 7: cout << "July";
+		break;
+	case 8: cout << "August";
+		break;
+	case 9:cout << "September";
+		break;
+	case 10: cout << "October";
+		break;
+	case 11: cout << "November";
+		break;
+	case 12:cout << "December";
+		break;
+	default: cout << "Error";
+		break;
+	}*/
+
+	/*int first, second, third;
+	cout << "Enter number 1: " << endl;
+	cin >> first;
+	cout << "Enter number 2: " << endl;
+	cin >> second;                                  Завдання 5(20)	
+	cin >> third;
+	cout << "Enter difference: " << endl;
+	int dif;
+	cin >> dif;
+	if (first + dif == second && second + dif == third)
+	{
+		cout << "algebraic sequence";
+	}
+	else
+		cout << "not algebraic sequence";*/
 }
